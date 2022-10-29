9ef3fdefde0e0ee454fe70f9c8b96560b62cffe5
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2022-10-29 19:27:21 +0100

    UPD - Gulp `libs:build` to also consider the package.json `main` as fallback

--- generators/app/templates/FrontEnd/gulp/libs.js
+++ generators/app/templates/FrontEnd/gulp/libs.js
@@ -34,11 +34,11 @@ async function build(done) {
 			continue;
 		}
 
-		let {files, exports} = JSON.parse(readFileSync(join('node_modules', dependency, 'package.json')));
+		let {files, exports, main} = JSON.parse(readFileSync(join('node_modules', dependency, 'package.json')));
 
 		files = typeof exports === 'object' && exports !== null && Array.isArray(exports) === false
 			? Object.values(exports)
-			: exports || files;
+			: exports || files || main || 'index.js';
 
 		files = globbySync(files, {
 			cwd: join('node_modules', dependency),
