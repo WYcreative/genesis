45f4e1e9aa32f9220ee14900d57ed6c5cc8b1121
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2022-11-29 14:04:11 +0000

    UPD - Fonts and Images to also be revisioned
    - TODO: Image fingerprints are currently reverted until BE is prepared to support them.

--- generators/app/templates/FrontEnd/gulp/fonts.js
+++ generators/app/templates/FrontEnd/gulp/fonts.js
@@ -1,7 +1,11 @@
+import {existsSync, readFileSync} from 'node:fs';
+
 import gulp from 'gulp';
+import rev from 'gulp-rev';
+import revRewrite from 'gulp-rev-rewrite';
 
 import config from '../config/index.js';
-import {getDirectory} from './utilities.js';
+import {getDirectory, getRelativePath} from './utilities.js';
 
 
 const {src, dest} = gulp;
@@ -14,13 +18,29 @@ function build() {
 
 
 function dist() {
-	return src(config.build.fonts)
-		.pipe(dest(getDirectory(config.dist.fonts)));
+	const manifest = existsSync(config.revManifest)
+		? readFileSync(config.revManifest)
+		: undefined;
+
+	return src(config.build.fonts, {
+		base: getDirectory(config.build.fonts, 2),
+	})
+		.pipe(rev())
+		.pipe(revRewrite({
+			manifest,
+			modifyUnreved: (path, {relative}) => getRelativePath(path, relative),
+			modifyReved: (path, {relative}) => getRelativePath(path, relative),
+		}))
+		.pipe(dest(getDirectory(config.dist.fonts, 2)))
+		.pipe(rev.manifest({
+			merge: true,
+		}))
+		.pipe(dest(getDirectory(config.revManifest)));
 }
 
 
 function backend() {
-	return src(config.build.fonts)
+	return src(config.dist.fonts)
 		.pipe(dest(getDirectory(config.backend.fonts)));
 }
 
--- generators/app/templates/FrontEnd/gulp/images.js
+++ generators/app/templates/FrontEnd/gulp/images.js
@@ -1,9 +1,15 @@
+import {existsSync, readFileSync} from 'node:fs';
+import {basename, extname} from 'node:path/posix';
+
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import imagemin from 'gulp-imagemin';
+import rev from 'gulp-rev';
+import rename from 'gulp-rename';
+import revRewrite from 'gulp-rev-rewrite';
 
 import config from '../config/index.js';
-import {getDirectory} from './utilities.js';
+import {getDirectory, getRelativePath} from './utilities.js';
 
 
 const {src, dest} = gulp;
@@ -18,8 +24,28 @@ function build() {
 
 
 function dist() {
-	return src(config.build.images)
-		.pipe(dest(getDirectory(config.dist.images[0])));
+	const manifest = existsSync(config.revManifest)
+		? readFileSync(config.revManifest)
+		: undefined;
+
+	return src(config.build.images, {
+		base: getDirectory(config.build.images, 2),
+	})
+		.pipe(rev())
+		// TODO: Remove this once BE finds a method for handling image file revisions.
+		.pipe(rename((path, {revOrigPath}) => {
+			path.basename = basename(revOrigPath, extname(revOrigPath));
+		}))
+		.pipe(revRewrite({
+			manifest,
+			modifyUnreved: (path, {relative}) => getRelativePath(path, relative),
+			modifyReved: (path, {relative}) => getRelativePath(path, relative),
+		}))
+		.pipe(dest(getDirectory(config.dist.images[0], 2)))
+		.pipe(rev.manifest({
+			merge: true,
+		}))
+		.pipe(dest(getDirectory(config.revManifest)));
 }
 
 
