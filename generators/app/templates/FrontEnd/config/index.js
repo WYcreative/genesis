15edb703a486115691065a2e8928793905b6d783
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2023-01-11 13:17:14 +0000

    FIX - XO linter error in template `config/index.js`

--- generators/app/templates/FrontEnd/config/index.js
+++ generators/app/templates/FrontEnd/config/index.js
@@ -2,7 +2,7 @@ import {createRequire} from 'node:module';
 import {join} from 'node:path/posix';
 
 
-// TODO [2022-10-25]: Use import assertions once they become stable, assuming they will be when Node 18 enters LTS mode.
+// TODO: Use import assertions once they become stable.
 const pkg = createRequire(import.meta.url)('../package.json');
 
 const data = './data';
